c7bf9c4246cdebcc1c83f9c1dc102ad4a80f9643
Krish26Jain <krishjain2609@gmail.com>
2024-07-10 09:48:45 +0530

    added some logic to show winner but error are present

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reser-btn");
+let newgameBtn = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true; 
 
@@ -14,6 +16,9 @@ const WinPatterns = [
     [6, 7, 8]
 ];
 
+
+
+
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         console.log("box was clicked");
@@ -26,5 +31,28 @@ boxes.forEach((box) => {
             turnO = true;
         }
         box.disabled = true;
+
+        CheckWinner();
     });
 });
+
+const ShowWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+};
+
+
+const CheckWinner = () => {
+    for(let pattern of WinPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;/*pattern [0] means Winpatterns ka 1st term played*/ 
+        let pos2Val = boxes[pattern[1]].innerText;/*pattern [0] means Winpatterns ka 1st term played*/ 
+        let pos3Val = boxes[pattern[2]].innerText;/*pattern [0] means Winpatterns ka 1st term played*/ 
+        
+        if(pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if(pos1Val === pos2Val && pos1Val === pos3Val) {
+                console.log("winner" , pos1Val);
+                ShowWinner(pos1Val);
+            }
+        }
+    }
+};
